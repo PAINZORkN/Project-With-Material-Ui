f3acfb0c55b75d2aadf63048e14042f68ec4b8fd
PAINZOR <painzor@outlook.it>
2022-08-11 00:24:57 +0200

    feat: add dropdown menu

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,77 +1,109 @@
-import { AppBar, Avatar, Badge, InputBase, Menu, MenuItem, styled, Toolbar, Typography } from '@mui/material'
-import React from 'react'
-import PetsIcon from '@mui/icons-material/Pets';
-import MailIcon from '@mui/icons-material/Mail';
-import { Box } from '@mui/system';
+import {
+  AppBar,
+  Avatar,
+  Badge,
+  InputBase,
+  Menu,
+  MenuItem,
+  styled,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import React from "react";
+import PetsIcon from "@mui/icons-material/Pets";
+import MailIcon from "@mui/icons-material/Mail";
+import { Box } from "@mui/system";
+// import BasicMenu from "./Menu";
+import { useState } from "react";
 
 const StyledToolbar = styled(Toolbar)({
-    display: "flex",
-    justifyContent: "space-between"
-})
+  display: "flex",
+  justifyContent: "space-between",
+});
 
-const Search = styled("div")(({ theme }) => ({
-    backgroundColor: "white",
-    padding: "0 10px",
-    borderRadius: theme.shape.borderRadius,
-    width: "45%",
-    [theme.breakpoints.up('sm')]: {
-        width: "30%",
-    }
-}))
+const Search = styled(Box)(({ theme }) => ({
+  backgroundColor: "white",
+  padding: "0 10px",
+  borderRadius: theme.shape.borderRadius,
+  width: "35%",
+  [theme.breakpoints.up("sm")]: {
+    width: "30%",
+  },
+}));
 
 const Icons = styled(Box)(({ theme }) => ({
-    display: "none",
-    alignItems: "center",
-    gap: "20px",
-    [theme.breakpoints.up('sm')]: {
-        display: "flex"
-    }
-}))
+  display: "none",
+  alignItems: "center",
+  gap: "20px",
+  [theme.breakpoints.up("sm")]: {
+    display: "flex",
+  },
+}));
 
 const UserBoxMobile = styled(Box)(({ theme }) => ({
-    display: "flex",
-    alignItems: "center",
-    gap: "10px",
-    [theme.breakpoints.up('sm')]: {
-        display: "none"
-    }
-}))
-
-
-
+  display: "flex",
+  alignItems: "center",
+  gap: "10px",
+  [theme.breakpoints.up("sm")]: {
+    display: "none",
+  },
+}));
 
 const Navbar = () => {
-    // const [open, setOpen] = useState(false)
-    return (
-        <AppBar position="sticky">
-            <StyledToolbar>
-                <Typography variant="h6" sx={{ display: { xs: "none", sm: "block" } }}>
-                    PAINZOR
-                </Typography>
-                <PetsIcon sx={{ display: { xs: "block", sm: "none" } }} />
-                <Search>
-                    <InputBase fullWidth={true} placeholder='Search...' />
-                </Search>
-                <Icons>
-                    <Badge badgeContent={4} color="error">
-                        <MailIcon />
-                    </Badge>
-                    <Badge color="secondary" badgeContent={100}>
-                        <MailIcon />
-                    </Badge>
-                    <Avatar
-                        sx={{ width: 35, height: 35 }} alt="Cindy Baker"
-                        src="/static/images/avatar/3.jpg" />
-                </Icons>
-                <UserBoxMobile>
-                    <Avatar
-                        sx={{ width: 35, height: 35 }} alt="Cindy Baker"
-                        src="/static/images/avatar/3.jpg" />
+  const [open, setOpen] = useState(false)
+  
+  return (
+    <AppBar position="sticky">
+      <StyledToolbar>
+        <Typography
+          variant="h6"
+          sx={{
+            display: { xs: "none", sm: "block" },
+          }}
+        >
+          META
+        </Typography>
+        <PetsIcon
+          sx={{
+            display: { xs: "block", sm: "none" },
+          }}
+        />
+        <Search>
+          <InputBase fullWidth={true} placeholder="Search..." />
+        </Search>
+        <Icons>
+          <Badge badgeContent={4} color="error">
+            <MailIcon />
+          </Badge>
+          <Badge color="secondary" badgeContent={100}>
+            <MailIcon />
+          </Badge>
+          <Avatar
+          sx={{
+            width: 35,
+            height: 35,
+          }}
+          alt="Cindy Baker"
+          src="/static/images/avatar/3.jpg"
+          onClick={(e)=>setOpen(true)}
+        />
+          {/* <BasicMenu /> */}
+        </Icons>
+        <UserBoxMobile  onClick={(e)=>setOpen(true)}>
+          <Avatar
+            sx={{
+              width: 35,
+              height: 35,
+            }}
+            alt="Cindy Baker"
+            src="/static/images/avatar/3.jpg"
+            
+          />
+          <Typography variant="span">John</Typography>
+        </UserBoxMobile>
+      </StyledToolbar>
 
-                    <Typography variant="span">John</Typography>
-                </UserBoxMobile>
-            </StyledToolbar>
-            {/* <Menu
+      <Menu
                 id="demo-positioned-menu"
                 aria-labelledby="demo-positioned-button"
                 open={open}
@@ -88,9 +120,9 @@ const Navbar = () => {
                 <MenuItem>Profile</MenuItem>
                 <MenuItem>My account</MenuItem>
                 <MenuItem>Logout</MenuItem>
-            </Menu> */}
-        </AppBar>
-    )
-}
+            </Menu>
+    </AppBar>
+  );
+};
 
-export default Navbar
+export default Navbar;
