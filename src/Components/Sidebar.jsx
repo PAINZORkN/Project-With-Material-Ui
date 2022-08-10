003989f2a6b873f6cf75f9047240758bf5a112b3
PAINZOR <painzor@outlook.it>
2022-08-11 00:54:17 +0200

    feat: added sidebar list items

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,10 +1,91 @@
-import { Box } from '@mui/material'
-import React from 'react'
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Switch,
+} from "@mui/material";
+import HomeIcon from "@mui/icons-material/Home";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
+import AutoStoriesIcon from "@mui/icons-material/AutoStories";
+import GroupsIcon from "@mui/icons-material/Groups";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import SettingsIcon from "@mui/icons-material/Settings";
+import SportsBarIcon from "@mui/icons-material/SportsBar";
+import StoreIcon from "@mui/icons-material/Store";
+import React from "react";
 
 const Sidebar = () => {
   return (
-    <Box backgroundColor="skyblue" flex={1} padding={2} sx={{display: {xs:"none", sm:"block"}}}>Sidebar</Box>
-  )
-}
+    <Box flex={1} padding={2} sx={{ display: { xs: "none", sm: "block" } }}>
+      <List>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#home">
+            <ListItemIcon>
+              <HomeIcon />
+            </ListItemIcon>
+            <ListItemText primary="Homepage" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#simple-list">
+            <ListItemIcon>
+              <AutoStoriesIcon />
+            </ListItemIcon>
+            <ListItemText primary="Pages" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#simple-list">
+            <ListItemIcon>
+              <GroupsIcon />
+            </ListItemIcon>
+            <ListItemText primary="Groups" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#simple-list">
+            <ListItemIcon>
+              <StoreIcon />
+            </ListItemIcon>
+            <ListItemText primary="Marketplace" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#simple-list">
+            <ListItemIcon>
+              <SportsBarIcon />
+            </ListItemIcon>
+            <ListItemText primary="Friends" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#simple-list">
+            <ListItemIcon>
+              <SettingsIcon />
+            </ListItemIcon>
+            <ListItemText primary="Settings" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#simple-list">
+            <ListItemIcon>
+              <AccountCircleIcon />
+            </ListItemIcon>
+            <ListItemText primary="Profile" />
+          </ListItemButton>
+        </ListItem>
+        <ListItemButton component="a" href="#simple-list">
+          <ListItemIcon>
+            <DarkModeIcon />
+          </ListItemIcon>
+          <Switch />
+        </ListItemButton>
+      </List>
+    </Box>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
