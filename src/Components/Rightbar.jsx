9a3ded0c910c0f574959484e3f731a918ecb40f4
unknown <sabinorotondi2000@gmail.com>
2022-08-12 00:48:31 +0200

    feat: added list items avatar

--- src/Components/Rightbar.jsx
+++ src/Components/Rightbar.jsx
@@ -1,4 +1,4 @@
-import { Avatar, AvatarGroup, Box, Typography } from '@mui/material'
+import { Avatar, AvatarGroup, Box, ImageList, ImageListItem, Typography } from '@mui/material'
 import React from 'react'
 
 const Rightbar = () => {
@@ -7,21 +7,59 @@ const Rightbar = () => {
       padding={2}
       sx={{ display: { xs: "none", sm: "block" } }}
     >
-
-
-      <Box position="fixed">
+      <Box position="fixed" width={300}>
         <Typography variant="h6" fontWeight={100}>Online Friends</Typography>
         <AvatarGroup max={7}>
           <Avatar alt="Remy Sharp" src="https://material-ui.com/static/images/avatar/1.jpg" />
           <Avatar alt="Travis Howard" src="https://material-ui.com//static/images/avatar/2.jpg" />
-          <Avatar alt="Cindy Baker" src="https://material-ui.com//static/images/avatar/3.jpg" />
-          <Avatar alt="Agnes Walker" src="https://material-ui.com//static/images/avatar/4.jpg" />
-          <Avatar alt="Trevor Henderson" src="https://material-ui.com//static/images/avatar/6.jpg" />
-          <Avatar alt="Trevor Henderson" src="https://material-ui.com//static/images/avatar/7.jpg" />
-          <Avatar alt="Trevor Henderson" src="https://material-ui.com//static/images/avatar/8.jpg" />
-          <Avatar alt="Trevor Henderson" src="https://material-ui.com//static/images/avatar/9.jpg" />
-          <Avatar alt="Trevor Henderson" src="https://material-ui.com//static/images/avatar/10.jpg" />
+          <Avatar alt="Cindy Baker" src="https://material-ui.com/static/images/avatar/3.jpg" />
+          <Avatar alt="Agnes Walker" src="https://material-ui.com/static/images/avatar/4.jpg" />
+          <Avatar alt="Trevor Henderson" src="https://material-ui.com/static/images/avatar/6.jpg" />
+          <Avatar alt="Trevor Henderson" src="https://material-ui.com/static/images/avatar/7.jpg" />
+          <Avatar alt="Trevor Henderson" src="https://material-ui.com/static/images/avatar/8.jpg" />
+          <Avatar alt="Trevor Henderson" src="https://material-ui.com/static/images/avatar/9.jpg" />
+          <Avatar alt="Trevor Henderson" src="https://material-ui.com/static/images/avatar/10.jpg" />
         </AvatarGroup>
+        <Typography variant="h6" fontWeight={100} mt={2} mb={2}>Latest Photos</Typography>
+        <ImageList cols={3} rowHeight={100} gap={5}>
+          <ImageListItem>
+            <img
+            src="https://material-ui.com/static/images/image-list/breakfast.jpg"
+            alt=""
+            />
+          </ImageListItem>
+          <ImageListItem>
+            <img
+            src="https://material-ui.com/static/images/image-list/burgers.jpg"
+            alt=""
+            />
+          </ImageListItem>
+          <ImageListItem>
+            <img
+            src="https://material-ui.com/static/images/image-list/camera.jpg"
+            alt=""
+            />
+          </ImageListItem>
+          <ImageListItem>
+            <img
+            src="https://material-ui.com/static/images/image-list/burgers.jpg"
+            alt=""
+            />
+          </ImageListItem>
+          <ImageListItem>
+            <img
+            src="https://material-ui.com/static/images/image-list/burgers.jpg"
+            alt=""
+            />
+          </ImageListItem>
+          <ImageListItem>
+            <img
+            src="https://material-ui.com/static/images/image-list/burgers.jpg"
+            alt=""
+            />
+          </ImageListItem>
+        </ImageList>
+        <Typography variant="h6" fontWeight={100} mt={2} mb={2}>Latest Conversations</Typography>
       </Box>
     </Box>
   )
